69abed1461cdba10bab645eadd03911d4356b05c
unknown <vardanyangugo3@gmail.com>
2023-07-29 01:58:36 +0400

    last change

--- src/modal/modalContent/index.js
+++ src/modal/modalContent/index.js
@@ -3,10 +3,9 @@ import { closeModal } from "../../store/showmodal";
 import "./index.css";
 
 function Modal({ chidren }) {
-  const dispatch = useDispatch();
   return (
     <>
-      <div onClick={() => dispatch(closeModal())} className="general">
+      <div className="general">
         <div className="card">{chidren}</div>
       </div>
     </>
