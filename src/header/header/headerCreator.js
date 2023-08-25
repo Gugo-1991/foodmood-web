d6ccd94e1379467788c5b5af12ce07e2ac6b2eeb
unknown <vardanyangugo3@gmail.com>
2023-08-26 00:15:06 +0400

    add admin profile fhoto

--- src/header/header/headerCreator.js
+++ src/header/header/headerCreator.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import "./style.css";
 import AdminModal from "../showAdminButtons";
 import Time from "./signUp/clock";
+import AdminFrofile from "../showAdminButtons/adminProfile";
 
 function Fixheader() {
   const user = useSelector(function (state) {
@@ -10,8 +11,11 @@ function Fixheader() {
 
   return (
     <>
-      <section className="header">
-        {user === "admin" ? <AdminModal /> : <Time />}
+      <section className="header d-flex position-fixed w-100  align-items-center ">
+        <div className="admin2 d-flex  w-100  align-items-center">
+          {user === "admin" ? <AdminModal /> : <Time />}
+          {user === "admin" ? <AdminFrofile /> : null}
+        </div>
       </section>
     </>
   );
